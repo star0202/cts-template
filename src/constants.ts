a1625ab2a7a0d7e0005a6302fb3c05e3e6e9ff56
Starcea <stardev.uwu@gmail.com>
2023-06-05 16:04:53 +0900

    refactor: version as string

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-export const VERSION = require('../package.json').version
+export const VERSION = require('../package.json').version as string
