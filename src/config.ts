1c47321c8f7ebc21a0b48c2ad5dc45604b388a8e
Starcea <stardev.uwu@gmail.com>
2023-06-24 22:47:47 +0900

    fix: sync command global when no guild config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 type Config = {
   token: string
-  guilds: string[]
+  guilds?: string[]
 }
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const config: Config = require('../config.json')
+if (config.guilds?.length === 0) config.guilds = undefined
