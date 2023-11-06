b121305ea3267dd0b3949074cb6f146e46ad7fdf
Starcea <stardev.uwu@gmail.com>
2023-11-06 22:02:19 +0900

    fix: colored hash

--- src/modules/Dev.ts
+++ src/modules/Dev.ts
@@ -49,7 +49,7 @@ class Dev extends CustomExt {
     )
 
     const guild = i.guild
-      ? `#${green((i.channel as GuildBasedChannel).name)}(${blue(
+      ? `${green(`#${(i.channel as GuildBasedChannel).name}`)}(${blue(
           i.channelId
         )} at ${green(i.guild.name)}(${blue(i.guild.id)})`
       : 'DM'
